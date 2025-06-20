02c1a52c8adc9996ee46a52d8db0831af61e7fe2
katafuchix <katafuchix@gmail.com>
2025-06-20 23:44:01 +0900

    Initial Commit

---
+++ LocationPickerExample/LocationPickerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationPickerExampleApp.swift
+//  LocationPickerExample
+//
+//  Created by cano on 2025/06/20.
+//
+
+import SwiftUI
+
+@main
+struct LocationPickerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
